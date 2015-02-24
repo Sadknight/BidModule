15c6ab074710be61cbffdcb81885cc0f233194ee
DMalenkov <den-malenkov@mail.ru>
2015-02-24 17:03:51 +0300

    This is main Sources for Print Manager Programm

--- src/staticClass/TestIntr.java
+++ src/staticClass/TestIntr.java
@@ -2,10 +2,12 @@ package staticClass;
 
 import model.Ad;
 
+import java.io.Serializable;
+
 /**
  * Created by DMalenkov on 24.02.2015.
  */
-public interface TestIntr {
+public interface TestIntr extends Serializable {
     public Ad getAd();
 
     public void setAd(Ad ad);
